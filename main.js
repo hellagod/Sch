a426e34d4b87058c6335aa8f5e7a20f5b0f10402
Samathpyth <kriss.sacha@gmail.com>
2020-04-15 04:18:24 +0300

    аа

--- main.js
+++ main.js
@@ -1,6 +1,76 @@
+
 ChooseTableType();
 CreateTableFirstType(0, 10000000000000);
+Header();
+init();
+ChooseMenuItem();
+function init() {
+    if (document.layers) document.captureEvents(Event.MOUSEMOVE);
+    document.onmousemove = mousemove;
+}
+function mousemove(event) {
+    let mouse_x = mouse_y = 0;
+    if (document.attachEvent != null) {
+        mouse_x = window.event.clientX;
+        mouse_y = window.event.clientY;
+    } else if (!document.attachEvent && document.addEventListener) {
+        mouse_x = event.clientX;
+        mouse_y = event.clientY;
+    }
+    let  menu = document.getElementById('menu');
+    if(mouse_x < 300){
+        menu.style.transform = 'translate(0, 0)';
+        menu.style.transition ='0.6s';
+    }
+    else{
+        menu.style.transform = 'translate(-250px, 0)';
+        menu.style.transition ='0.6s';
+    }
+}
+function ChooseMenuItem(){
+    let array = document.getElementsByClassName('item');
+    let last = 0;
+    for (let i = 0; i < array.length; i++) {
+        let y = array[i];
+        array[i].onmouseover = function () {
+            y.style.background = "rgba(101,108,103,0.29)";
+            y.style.transition = '0.6s';
+            y.style.paddingLeft = '50px';
 
+        };
+        array[i].onmouseout = function () {
+            y.style.background = "rgba(187,195,190,0.29)";
+            y.style.transition = '0.6s';
+            y.style.paddingLeft = '20px';
+        };
+        array[i].onmousedown = function () {
+            switch (i) {
+                case 0:
+                    let tbg = document.getElementById('tableid');
+                    let signin = document.getElementById('signin');
+                    tbg.style.zIndex = '100000';
+                    signin.style.zIndex = '0';
+            }
+        }
+    }
+}
+function Header() {
+    if(true){
+        let header = document.getElementById('header');
+        let button = document.createElement('input');
+        button.type = 'button';
+        button.value = 'войти';
+        button.onclick = function(){
+            let tbg = document.getElementById('tableid');
+            let signin = document.getElementById('signin');
+            tbg.style.zIndex = '0';
+            signin.style.zIndex = '1000000';
+        }
+        button.appendChild(document.createTextNode('Войти'));
+        button.className = 'button_for_login_in_the_header';
+        header.appendChild(button);
+    }
+}
 function ChooseTableType() {
     let g1 = document.getElementById('g1');
     let g2 = document.getElementById('g2');
@@ -27,7 +97,6 @@ function ChooseTableType() {
         array[i].onmousedown = function () {
             last = j;
             let tbl = document.getElementsByClassName("table");
-            CreateTableFirstType(0, 10000000000000);
         }
     }
 }
@@ -41,6 +110,8 @@ function login() {
     let con = new XMLHttpRequest();
     if (login && pass) {
         con.open('GET', `http://shproj2020.herokuapp.com/login?username=${login}&password=${sha256(pass)}`, false);
+        con.send();
+        con.open('GET', `http://shproj2020.herokuapp.com/get_prs_id`, false);
         con.send();
     }
 }
